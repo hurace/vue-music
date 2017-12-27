b9630f23fed44a6e2e5a869eec5e5961409707e6
hurace <1101037077@qq.com>
2017-12-27 21:40:32 +0800

    7-3 player

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,20 @@
         <m-header></m-header>
         <tab></tab>
         <router-view></router-view>
+        <player></player>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
   import MHeader from 'components/m-header/m-header'
   import Tab from 'components/tab/tab'
+  import Player from 'components/player/player'
 
   export default {
     components: {
       MHeader,
-      Tab
+      Tab,
+      Player
     }
   }
 </script>
